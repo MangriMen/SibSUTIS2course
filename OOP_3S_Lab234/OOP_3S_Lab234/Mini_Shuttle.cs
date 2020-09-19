e847bc3f9a6fe50fd7b4a456de68cbe7f14f5add
MangriMen <mangrimen@gmail.com>
2020-09-19 13:56:37 +0600

    Added base shuttle class and Mini_shuttle

---
+++ OOP_3S_Lab234/OOP_3S_Lab234/Mini_Shuttle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_3S_Lab234
+{
+    class Mini_Shuttle : Shuttle
+    {
+        Mini_Shuttle()
+        {
+            Speed = 20;
+            
+        }
+    }
+}
--- OOP_3S_Lab234/OOP_3S_Lab234/Shuttle.cs
+++ OOP_3S_Lab234/OOP_3S_Lab234/Shuttle.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace OOP_3S_Lab234
 {
     class Shuttle
     {
+        protected Vector<double> velocity;
+
+        protected Vector<double> coords;
+
+        protected int speed_ = new int();
+
+        protected Texture2D texture_;
+
+        protected Texture2D Texture
+        {
+            get { return texture_; }
+            set { texture_ = value; }
+        }
+        public int Speed
+        {
+            get { return speed_; }
+            set { speed_ = value > 0 ? value : 0; }
+        }
     }
 }
